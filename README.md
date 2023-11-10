b81766c9c7f758189ebbe656299e31b2ae82edb9
Thomas Brewer <tom.brewer@mac.com>
2023-11-10 05:56:36 -0500

    adding posts controller and basic integration with eventrelay (#1)
    * adding posts controller and basic integration with eventrelay
    
    * making ngrok host dynamic in development

--- README.md
+++ README.md
@@ -19,6 +19,30 @@ Things you may want to cover:
 
 * Services (job queues, cache servers, search engines, etc.)
 
+### Initial Setup of EventRelay
+
+In development you can start EventRelay without authentication for the GRPC API by setting `ER_SKIP_GRPC_AUTH=true`
+
+Start EventRelay:
+
+`ER_SKIP_GRPC_AUTH=true mix phx.server`
+
+Once EventRelay is up and running you can use grpcurl to create the topic needs for this Rails app.
+
+```bash
+grpcurl -plaintext -proto event_relay.proto -d '{"name": "posts"}' localhost:50051 eventrelay.Topics.CreateTopic
+```
+
+```json
+{
+  "topic": {
+    "id": "a9cc1872-363d-45c3-9de1-ae9a1d3a9082",
+    "name": "posts"
+  }
+}
+```
+
+
 * Deployment instructions
 
 * ...
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,14 +8,19 @@ Bundler.require(*Rails.groups)
 
 module EventrelayRubyProducer
   class Application < Rails::Application
+    grpc_root = File.join(config.root, '/lib/ruby_protos')
+    $LOAD_PATH.unshift(grpc_root)
+    Dir[File.join(grpc_root, '**/*.rb')].each do |file|
+      require file
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-
+    config.autoload_lib(ignore: %w[assets tasks])
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
